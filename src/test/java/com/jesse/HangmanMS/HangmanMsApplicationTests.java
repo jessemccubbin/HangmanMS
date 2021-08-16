121eb50463f317cb71fb5345d22f35a9df5513b0
Jesse Mccubbin <jessemccubbin1@gmail.com>
2021-08-17 08:24:44 +1000

    Initial commit

---
+++ src/test/java/com/jesse/HangmanMS/HangmanMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesse.HangmanMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
