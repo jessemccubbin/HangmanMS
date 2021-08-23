65c23bd7d10e3e392d92a19385750b6e3cd86938
Jesse Mccubbin <jessemccubbin1@gmail.com>
2021-08-23 16:32:21 +1000

    changes to test

--- src/test/java/com/jesse/HangmanMS/HangmanMsApplicationTests.java
+++ src/test/java/com/jesse/HangmanMS/HangmanMsApplicationTests.java
@@ -4,11 +4,15 @@ import com.jesse.HangmanMS.controller.HangmanController;
 import com.jesse.HangmanMS.model.HangmanGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -25,18 +29,20 @@ class HangmanMsApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@InjectMocks
+	private HangmanGame hangmanGame;
+
 	@BeforeEach
-	void setup() {
-		//ame.setWord("apple");
+	public void setup() throws Exception{
+		mockMvc = MockMvcBuilders.standaloneSetup("apple")
+				.build();
 	}
 
-
 	@Test
-	@GetMapping("/")
-	public void shouldReturnDefaultMessage(Model model) throws Exception {
-
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("apple")));
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/")
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("apple")));
 	}
 
 	@Test
